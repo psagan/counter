e77492a3c2741c3bc28fc90ea298cd01dc6659f8
psagan <patryk.sagan@icloud.com>
2020-09-23 12:42:21 +0200

    initial commit

---
+++ lib/clock.ex
@@ -0,0 +1,13 @@
+defmodule Clock do
+
+  def start(f) do
+    run(f, 0)
+  end
+
+  def run(your_hears_desire, count) do
+    your_hears_desire.(count)
+    new_count = Counter.Core.inc(count)
+    :timer.sleep(1000)
+    run(your_hears_desire, new_count)
+  end
+end
---
+++ lib/counter/core.ex
@@ -0,0 +1,7 @@
+defmodule Counter.Core do
+
+  def inc(value) do
+    value + 1
+  end
+
+end
